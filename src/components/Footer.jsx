7854d9ed578cd7fe25b4364ede06a963872ada24
opeyemi <opeyemi@gmail.com>
2024-06-29 19:55:01 +0100

    sun

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,12 +6,12 @@ import { FaTiktok } from "react-icons/fa6";
 function Footer() {
   return (
    <>
-     <div className="bg-orange-500 py-10px px-5px md:px-10 flex flex-col md:flex-col justify-between">
+     <div className="bg-orange-500 py-10 px-5 md:px-10 flex flex-col md:flex-row justify-between">
         <div className="flex items-center justify-center md:justify-start mb-5 md:mb-0">
             <p className="text-[24px] lg:text-[30px] font-bold">TECHNOTRONIX</p>
         </div>
         <div className="text-center md:text-left mb-5 md:mb-0">
-            <h1 className="text-[18px] font-bold mb-[10px]">Useful Links</h1>
+            <h1 className="text-[18px] font-bold mb-2">Useful Links</h1>
             <ul>
                 <li> <a  className="hover:text-white" href="">Home</a> </li>
                 <li> <a className="hover:text-white"   href="">Contact</a> </li>
@@ -19,8 +19,8 @@ function Footer() {
                 <li> <a  className="hover:text-white"  href="">Terms and Condition</a> </li>
             </ul>
         </div>
-        <div className="text-center ">
-            <h1 className="text-[18px] font-bold mb-[10px] ">Follow Us</h1>
+        <div className="text-center  md:text-left">
+            <h1 className="text-[18px] font-bold mb-2 ">Follow Us</h1>
             <div className="flex gap-5 justify-center md:justify-start">
               <FaFacebook/> 
               <FaXTwitter/>  
@@ -29,7 +29,7 @@ function Footer() {
             </div>
         </div>
     </div>
-    <div className="bg-black text-white text-center py-[5px]">
+    <div className="bg-black text-white text-center py-2">
         <p>&copy; Copyright Technotronix | All rights reserved.</p>
     </div>
    </>
